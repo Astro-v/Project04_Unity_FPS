a7c003e3e8f3145b10d17103e014147b2f86fef1
astro-v <valentinmichel07200@gmail.com>
2022-11-27 16:58:31 +0100

    Update
    Add the camera movement and the player movement by adding two C# script named PlayerController.cs and PlayerMotor.cs.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))] //  PlayerController can't work without PlayerMotors (it's required) - it is a good way to do to be sure that every componant that have their dependency
+public class PlayerController : MonoBehaviour
+{
+    #region Properties
+    [SerializeField] private float _speed = 3f;
+    [SerializeField] private float _mouseSensitivityX = 3f;
+    [SerializeField] private float _mouseSensitivityY = 3f;
+
+    private PlayerMotor _motor;
+    #endregion Properties
+
+    #region Methods
+    private void Start()
+    {
+        _motor = GetComponent<PlayerMotor>();
+    }
+
+    private void Update()
+    {
+        /* FOR THE PLAYER MOVEMENT */
+
+        // Compute velocity (speed) of the player's movement
+        float xMov = Input.GetAxisRaw("Horizontal"); // GetAxisRaw has no filter (it is the true data) and GetAxis apply a filter // We get -1 with the D key and +1 with the Q key
+        float zMov = Input.GetAxisRaw("Vertical"); // We get -1 with the S key and +1 with the Z key
+
+        Vector3 moveHorizontal = transform.right * xMov; // transform.right give the axes (transform.left work aswell)
+        Vector3 moveVertical = transform.forward * zMov;
+
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * _speed; // Direction (normalized) times the speed
+
+        _motor.Move(velocity);
+
+        /* FOR THE PLAYER ROTATION */
+
+        float yRot = Input.GetAxisRaw("Mouse X"); // Rotation around the y rotation, it match with the X movement of the mouse
+
+        Vector3 rotation = new Vector3(0, yRot, 0).normalized * _mouseSensitivityX;
+
+        _motor.Rotate(rotation);
+
+        /* FOR THE CAMERA ROTATION */
+
+        float xRot = Input.GetAxisRaw("Mouse Y"); // Rotation around the x rotation, it match with the Y movement of the mouse
+
+        Vector3 cameraRotation = new Vector3(xRot, 0, 0).normalized * _mouseSensitivityY;
+
+        _motor.RotateCamera(cameraRotation);
+    }
+    #endregion Methods
+}
--- Assets/PlayerMotor.cs
+++ Assets/PlayerMotor.cs
@@ -1,18 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))] //  PlayerMotor can't work without Rigidbody (it's required) - it is a good way to do to be sure that every componant that have their dependency
 public class PlayerMotor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Properties
+    [SerializeField] private Camera _camera;
+
+    private Vector3 _velocity = Vector3.zero;
+    private Vector3 _rotation = Vector3.zero;
+    private Vector3 _cameraRotation = Vector3.zero;
+
+    private Rigidbody _rb = null;
+    #endregion Properties
+
+    #region Methods
+    private void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Move(Vector3 velocity)
     {
-        
+        _velocity = velocity;
     }
+
+    public void Rotate(Vector3 rotation)
+    {
+        _rotation = rotation;
+    }
+
+    public void RotateCamera(Vector3 cameraRotation)
+    {
+        _cameraRotation = cameraRotation;
+    }
+
+    private void FixedUpdate() // More acces on physics than Update. Do not depend on the framerate
+    {
+        PerformMovement();
+        PerformRotation();
+    }
+
+    private void PerformMovement()
+    {
+        if (_velocity != Vector3.zero)
+        {
+            _rb.MovePosition(_rb.position + _velocity * Time.fixedDeltaTime); // _rb.position or transform.position are exactly the same in that precise case
+        }
+    }
+
+    private void PerformRotation()
+    {
+        _rb.MoveRotation(_rb.rotation * Quaternion.Euler(_rotation)); // MoveRotation work with Quaternion and not Vector.
+        _camera.transform.Rotate(-_cameraRotation);
+    }
+    #endregion Methods
 }
